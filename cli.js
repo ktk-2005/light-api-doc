04cbc6c4a5da59b928e11642c008ac31bce39e25
bqqbarbhg <samuli.1995@hotmail.com>
2018-11-18 17:16:04 +0200

    Add CLI separation

---
+++ cli.js
@@ -0,0 +1,42 @@
+#!/usr/bin/env node
+
+const ArgumentParser = require('argparse').ArgumentParser
+const main = require('./main')
+
+const parser = new ArgumentParser({
+  version: '0.1.0',
+  addHelp: true,
+  description: 'Lightweight API documentation generator',
+})
+
+parser.addArgument('-t', {
+  help: 'Template filename',
+  metavar: 'template.md',
+  required: true,
+})
+
+parser.addArgument('-o', {
+  help: 'Output filename',
+  metavar: 'output.md',
+  required: true,
+})
+
+parser.addArgument('--outdir', {
+  help: 'Output directory for relative links (overrides -o)',
+})
+
+parser.addArgument('--pattern', {
+  help: 'Regex pattern the files must match',
+  defaultValue: '\.js$',
+})
+
+parser.addArgument('dir', {
+  help: 'Source search directory',
+})
+
+const args = parser.parseArgs()
+main(args).catch((error) => {
+  console.error(error)
+  process.exitCode = 1
+})
+
